766be07060ef7542c7a5a00f45996dad9309583c
Marija Kalebota <marija.kalebota@gmail.com>
2018-05-03 12:59:43 +0200

    Add getters

--- FIVOA/Constraints/ExplicitConstraintForOneDimension.py
+++ FIVOA/Constraints/ExplicitConstraintForOneDimension.py
@@ -3,8 +3,14 @@ class ExplicitConstraintForOneDimension(object):
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
+    def get_lower_bound(self):
+        return self.lower_bound
+
+    def get_upper_bound(self):
+        return self.upper_bound
+
     def is_satisfied(self, value):
-        if(value > self.upper_bound or value < self.lower_bound):
+        if(value > self.get_upper_bound() or value < self.get_lower_bound()):
             return False
         else:
             return True

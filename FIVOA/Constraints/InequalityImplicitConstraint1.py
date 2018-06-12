af7222df72e8996e0202da2fedbb875c8d59c6d5
Marija Kalebota <marija.kalebota@gmail.com>
2018-06-12 18:22:15 +0200

    Fix minor bug

--- FIVOA/Constraints/InequalityImplicitConstraint1.py
+++ FIVOA/Constraints/InequalityImplicitConstraint1.py
@@ -2,13 +2,13 @@ from IInequalityImplicitConstraint import *
 
 class InequalityImplicitConstraint1(IInequalityImplicitConstraint):
     def is_satisfied(self, point):
-        if(point.getElement(0,1) - point.getElement(0,0) >= 0):
+        if(point.get_value_at_dimension(1) - point.get_value_at_dimension(0) >= 0):
             return True
         else:
             return False
 
     def value_at(self, point):
-        return point.getElement(0,1) - point.getElement(0,0)
+        return point.get_value_at_dimension(1) - point.get_value_at_dimension(0)
 
     def get_gradient(self):
         raise NotImplementedError
--- FIVOA/Constraints/InequalityImplicitConstraint2.py
+++ FIVOA/Constraints/InequalityImplicitConstraint2.py
@@ -2,13 +2,13 @@ from IInequalityImplicitConstraint import *
 
 class InequalityImplicitConstraint2(IInequalityImplicitConstraint):
     def is_satisfied(self, point):
-        if(2 - point.getElement(0,0) >= 0):
+        if(2 - point.get_value_at_dimension(0) >= 0):
             return True
         else:
             return False
 
     def value_at(self, point):
-        return 2 - point.getElement(0,0)
+        return 2 - point.get_value_at_dimension(0)
 
     def get_gradient(self):
         raise NotImplementedError

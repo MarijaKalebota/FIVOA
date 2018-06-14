f770cdbf0a67cc5859bb68a9f71eb1222e959033
Marija Kalebota <marija.kalebota@gmail.com>
2018-06-14 14:35:18 +0200

    Fix minor bug

--- FIVOA/Constraints/EqualityImplicitConstraint4.py
+++ FIVOA/Constraints/EqualityImplicitConstraint4.py
@@ -2,7 +2,7 @@ from IEqualityImplicitConstraint import *
 
 class EqualityImplicitConstraint4(IEqualityImplicitConstraint):
     def is_satisfied(self, point):
-        if (point.get_value_at_dimension(1) - point.getElement(0) + 50 == 0):
+        if (point.get_value_at_dimension(1) - point.get_value_at_dimension(0) + 50 == 0):
             return True
         else:
             return False

92c8ae867a003669beccc5e5189338484dfaf697
Marija Kalebota <marija.kalebota@gmail.com>
2018-06-24 10:27:29 +0200

    Add counter incrementation

--- FIVOA/Functions/F3OneDimensional.py
+++ FIVOA/Functions/F3OneDimensional.py
@@ -3,7 +3,7 @@ from IFunction import *
 class F3OneDimensional(IFunction):
 
     def value_at(self, point):
-        #self.increment_number_of_calls()
+        self.increment_number_of_calls()
         return (point.get_value_at_dimension(0) - 3) ** 2
 
     def gradient_at(self, point):
